824b2b89682398e628efdbbf3a16f213848f17fa
Eduardo <eduardo.alonso.73h@gmail.com>
2022-10-18 15:29:35 -0500

    We configure the viewModel  creating the variables  that will be observed

--- app/src/main/java/com/example/weather/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/weather/mainModule/viewModel/MainViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.weather.mainModule.viewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weather.R
 
-class MainViewModel {
+import com.example.weather.common.entities.WeatherForecastEnt
+import com.example.weather.mainModule.model.MainRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel:ViewModel() {
+    private val repository = MainRepository()
+
+    private  val result=MutableLiveData<WeatherForecastEnt>()
+    fun getResult():LiveData<WeatherForecastEnt> = result
+
+
+    private val snapshotMgs=MutableLiveData<Int>()
+    fun getSnapshotMgs() = snapshotMgs
+
+    private  val loader=MutableLiveData<Boolean>()
+    fun isLoading()= loader
+
+    suspend fun getWeatherForecast(lat:Double,Log:Double,appId:String,units:String,lang:String){
+        viewModelScope.launch {
+            try {
+                loader.value=true
+                val resultSever=repository.getWeatherForecast(lat,Log,appId,units,lang)
+                result.value=resultSever
+            }catch (e:Exception){
+                snapshotMgs.value= R.string.main_error_server
+
+            }finally {
+                loader.value=false
+            }
+        }
+    }
+
+
+
 }
