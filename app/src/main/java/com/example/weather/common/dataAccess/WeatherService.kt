382d7a9d431574621c6b51de7770420351a68a00
Eduardo <eduardo.alonso.73h@gmail.com>
2022-10-18 13:03:22 -0500

    We create the interface with the getWeatherForecastByCoodinates function with the GET annotation

---
+++ app/src/main/java/com/example/weather/common/dataAccess/WeatherService.kt
@@ -0,0 +1,18 @@
+package com.example.weather.common.dataAccess
+
+import com.example.weather.common.WeatherForecastEnt
+import com.example.weather.common.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+    @GET(Constants.ONE_CALL_PATH)
+    suspend fun getWeatherForecastByCoodinates(
+        @Query(Constants.LATITUDE_PARAM) lat: Double,
+        @Query(Constants.LONGITUDE_PARAM) lot: Double,
+        @Query(Constants.APP_ID_PARAM) appId: String,
+        @Query(Constants.UNITS_PARAM) unists: String,
+        @Query(Constants.LANGUAGE_PARAM) lang: String,
+        
+        ): WeatherForecastEnt
+}
