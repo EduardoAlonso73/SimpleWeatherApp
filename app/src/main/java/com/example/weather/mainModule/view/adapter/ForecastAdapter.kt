13e81d6f3c199046ca3ea9e2798068633453fd3c
Eduardo <eduardo.alonso.73h@gmail.com>
2022-10-20 10:26:15 -0500

    We create the ListAdapter and viewHolde with implentation of DataBinding

--- app/src/main/java/com/example/weather/mainModule/view/adapter/ForecastAdapter.kt
+++ app/src/main/java/com/example/weather/mainModule/view/adapter/ForecastAdapter.kt
@@ -1,4 +1,48 @@
 package com.example.weather.mainModule.view.adapter
 
-class ForecastAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weather.BR
+import com.example.weather.R
+import com.example.weather.common.entities.HourlyForecast
+import com.example.weather.databinding.ItemWeatherBinding
+
+
+class ForecastAdapter(private val listener: onClickListener) :
+    ListAdapter<HourlyForecast, RecyclerView.ViewHolder>(ForecastDiffCallbacks()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return  ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_weather,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        val forecast=getItem(position)
+        with(holder as ViewHolder){
+            holder.binding?.setVariable(BR.current,forecast)
+            holder.binding?.executePendingBindings()
+        }
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = DataBindingUtil.bind<ItemWeatherBinding>(view)
+        fun setListener(forecast: HourlyForecast) {
+            /*   binding?.root?.setOnClickListener { listener.onClick(itemEntity) }
+               binding.cbDelete.setOnClickListener{ listener.onLongClick(itemEntity) }*/
+        }
+    }
+
+}
+
+class ForecastDiffCallbacks : DiffUtil.ItemCallback<HourlyForecast>() {
+
+    override fun areItemsTheSame(oldItem: HourlyForecast, newItem: HourlyForecast): Boolean =oldItem.dt==newItem.dt
+
+    override fun areContentsTheSame(oldItem: HourlyForecast, newItem: HourlyForecast): Boolean =oldItem.dt==newItem.dt
 }
---
+++ app/src/main/java/com/example/weather/mainModule/view/adapter/onClickListener.kt
@@ -0,0 +1,4 @@
+package com.example.weather.mainModule.view.adapter
+
+interface onClickListener {
+}
